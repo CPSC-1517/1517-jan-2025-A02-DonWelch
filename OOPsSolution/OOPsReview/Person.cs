78b8ce3a5470f704c9e2fdaf348549ed4922bc08
Don Welch <dwelch@nait.ca>
2025-01-23 16:41:14 -0700

    cover [Theory] and [inlineData] with test method header, outline property tests, code collection exception testing

--- OOPsSolution/OOPsReview/Person.cs
+++ OOPsSolution/OOPsReview/Person.cs
@@ -24,6 +24,10 @@ namespace OOPsReview
         public Person(string firstname, string lastname, ResidentAddress address,
                         List<Employment> employments)
         {
+            if (string.IsNullOrWhiteSpace(firstname))
+                throw new ArgumentNullException("First Name", "First Name is missing. Data required.");
+            if (string.IsNullOrWhiteSpace(lastname))
+                throw new ArgumentNullException("Last Name", "Last Name is missing. Data required."); 
             FirstName = firstname;
             LastName = lastname;
             Address = address;
