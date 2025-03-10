5cfe8f77255bdbf8ea0291690c12b5a557dc03b9
Don Welch <dwelch@nait.ca>
2025-03-10 08:52:40 -0600

    create app lib services to query Region table

--- CSSolutionn/WestWindSystem/WestWindExtensions.cs
+++ CSSolutionn/WestWindSystem/WestWindExtensions.cs
@@ -53,7 +53,11 @@ namespace WestWindSystem
                 return new BuildVersionServices(context);
             });
 
-            
+            services.AddTransient<RegionServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+                return new RegionServices(context);
+            });
         }
     }
 }
